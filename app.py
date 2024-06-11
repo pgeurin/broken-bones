a694806824bf2c2aeafac050c3e683544c9948e1
Philip Geurin <philip.geurin@gmail.com>
2024-06-11 12:20:45 -0700

    add app, requirements, ipynb, model

---
+++ app.py
@@ -0,0 +1,27 @@
+# import gradio as gr
+
+# def greet(name):
+#     return "Hello " + name + "!!"
+
+# demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# demo.launch()
+
+from fastai.vision.all import *
+import gradio as gr
+
+def is_cat(x): return x[0].isupper()
+
+learn = load_learner('bone-model.pkl')
+
+categories = ('bone', 'broken bone')
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+image = gr.Image(type='pil')
+label = gr.Label()
+examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch()
